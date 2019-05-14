69317919143415ace56eef801fb347a68c3f5f67
ZahraNaddaf <naddaf.sajedeh@gmail.com>
2019-05-14 15:17:51 +0430

    test tuple type from Terminal

--- file 1.py
+++ file 1.py
@@ -5,5 +5,8 @@ dic1= {"id":1234, "Name": "Zahra", "Lastname": "Naddaf"}
 print(dic1["Name"])
 
 lst=[1, 2, 1001, 1002, "H", 5, 4, 3, 2, 1]
-print(lst[1-:-6:-1])
+print(lst[-1:-6:-1])
+
+tpl = (1, 5 ,87 , 94)
+print (type(tpl))
 
