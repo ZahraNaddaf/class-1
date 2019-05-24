1b491f278e11b23a8ec52cfd9b4ae4b80c0366cf
Student <zahranaddaf>
2019-05-24 10:11:03 +0330

    modify

--- session3/guess.py
+++ session3/guess.py
@@ -5,10 +5,10 @@ while True:
     answer = input("guess:",)
     answer = int(answer)
     if answer > secret:
-        print("secret is greater than your number")
+        print("secret is lesser than your number")
 
-    if answer < secret:
-       print("secret is lesser than your number")
+    elif answer < secret:
+       print("secret is greater than your number")
 
-    if answer == secret:
+    elif answer == secret:
         print("True")
