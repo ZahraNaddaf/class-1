6c3bb3b002a1b9c75bf1b602be89863e437d1eb1
Student <naddaf.sajedeh@gmail.com>
2019-06-14 11:52:04 +0330

    fibonacci_function

--- session_4/fibonacci_function.py
+++ session_4/fibonacci_function.py
@@ -1,10 +1,13 @@
 def fibonacci(n):
-    if n in (1, 2):
+    if n in (0, 1):
         return 1
 
     res = fibonacci(n - 1) + fibonacci(n - 2)
     return res
 
 
-for i in range (1, 20):
+for i in range(1, 20):
     print(fibonacci(i))
+
+
+print(fibonacci(3))
