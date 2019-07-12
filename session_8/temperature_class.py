56456315aad8da2f799907d8b34d4679751c72d5
Student <naddaf.sajedeh@gmail.com>
2019-07-12 09:28:16 +0330

    tempereture_class_add_doctest

--- session_8/temperature_class.py
+++ session_8/temperature_class.py
@@ -4,25 +4,27 @@ class Tempreture:
     -------
     >>> t = Tempreture(27)
     >>> t.cels
-    27
+    27.0
     >>> t.fahrenheit
     80.6
-
+    >>> t.fahrenheit = 100
+    37.77777777777778
 
     """
-    def __init__(self,celsius):
+
+    def __init__(self, celsius):
         self.cels = celsius
 
     @property
     def fahrenheit(self):
-        return (9/5)*self.cels + 32
+        return (9 / 5) * self.cels + 32
 
     @fahrenheit.setter
     def fahrenheit(self, value):
-        self.cels = (5/9)*(value - 32)
+        self.cels = (5 / 9) * (value - 32)
+
 
 if __name__ == '__main__':
     import doctest
-
 
     doctest.testmod()
