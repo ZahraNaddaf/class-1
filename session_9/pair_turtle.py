262cff8ef934157954bb9ef493b82c2b6edb440f
Student <naddaf.sajedeh@gmail.com>
2019-07-19 11:05:50 +0330

    new_new_turtle_class

---
+++ session_9/pair_turtle.py
@@ -0,0 +1,44 @@
+import turtle
+import math
+
+
+print(type(turtle.Turtle))
+
+
+class NewTurtle(turtle.Turtle):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.speed(8)
+        self.__dude = None
+
+    @property
+    def dude(self):
+        return self.__dude
+
+    @dude.setter
+    def dude(self, value):
+        self.__dude = value
+        if self.dude.dude != self:
+            self.dude.dude = self
+
+    def turn(self):
+        for i in range(4):
+            self.forward(100)
+            self.left(90)
+
+    def come_closer(self):
+        x1, y1 = self.pos()
+        x2, y2 = self.__dude.pos()
+        dx = x2 - x1
+        dy = y2 - y1
+        t = math.atan2(dy, dx)
+        d = math.degrees(t)
+        self.setheading(d)
+        self.__dude.setheading(180 + d)
+
+
+
+nt1 = NewTurtle()
+nt2 = NewTurtle()
+
+
